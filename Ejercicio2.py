bb06f8bb853a308d6cca6ce571c99ce3f7c5f109
Sbastdia <sbastdia@myuax.com>
2021-10-15 16:55:04 +0200

    Ejercicio3

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -3,12 +3,12 @@ class Animal:
     color=""
     nombre=""
 
-    def __init__(self,tam,color): #constructor de la clase
+    def __init__(self,tam=5,color="azul"): #constructor de la clase
         self.tam=tam
         self.color=color
         print ("Nacio un Animal")
 
-    def setNombre(self,nombre):
+    def setNombre(self,nombre="pantera"):
         self.nombre=nombre
 
     def getTamano(self):
@@ -23,36 +23,36 @@ class Animal:
     def correr(self):
         print ("El animal corre")
 
-class Gato(Animal):
+# class Gato(Animal):
 
-    def __init__(self,tam,color):
-        self.tam=tam
-        self.color=color
-        print ("Nacio un Gato\n")
+#     def __init__(self,tam,color):
+#         self.tam=tam
+#         self.color=color
+#         print ("Nacio un Gato\n")
 
-    def maullar(self):
-        print ("El gato maulla\n")
+#     def maullar(self):
+#         print ("El gato maulla\n")
 
-class Perro(Animal):
-    def __init__(self,tam,color):
-        self.tam=tam
-        self.color=color
-        print ("Nacio un Perro\n")
+# class Perro(Animal):
+#     def __init__(self,tam,color):
+#         self.tam=tam
+#         self.color=color
+#         print ("Nacio un Perro\n")
 
-    def ladrar(self):
-        print ("El perro ladra\n")
+#     def ladrar(self):
+#         print ("El perro ladra\n")
 
-#Programa Principal
-gato = Gato(10,"Negro")
-gato.setNombre("Mustafar")
-gato.maullar()
-print ("Su nombre es "+gato.getNombre())
-print (" mide "+str(gato.getTamano()))
-print ("su color es "+gato.getColor()+"\n")
+# #Programa Principal
+# gato = Gato(10,"Negro")
+# gato.setNombre("Mustafar")
+# gato.maullar()
+# print ("Su nombre es "+gato.getNombre())
+# print (" mide "+str(gato.getTamano()))
+# print ("su color es "+gato.getColor()+"\n")
 
-perro = Perro(20,"Blanco")
-perro.setNombre("Lacie")
-perro.ladrar()
-print ("Su nombre es "+perro.getNombre())
-print (" mide "+str(perro.getTamano()))
-print ("su color es "+perro.getColor()+"\n")
+# perro = Perro(20,"Blanco")
+# perro.setNombre("Lacie")
+# perro.ladrar()
+# print ("Su nombre es "+perro.getNombre())
+# print (" mide "+str(perro.getTamano()))
+# print ("su color es "+perro.getColor()+"\n")
